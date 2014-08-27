5db3dfa5c8ca2d849ac0480e19ef5905a5b5b944
Lee Young Jin <me.yjlee@gmail.com>
2014-08-27 10:13:35 +0900

    h2 데이터베이스 적용

--- jpa-sample/src/test/java/TestJpa.java
+++ jpa-sample/src/test/java/TestJpa.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -7,11 +8,12 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import javax.transaction.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = "dev")
 @ContextConfiguration(locations={
         "classpath*:META-INF/spring/context-application.xml",
-        "classpath*:META-INF/spring/context-datasource-hsql.xml",
+        "classpath*:META-INF/spring/context-datasource-h2.xml",
         "classpath*:META-INF/spring/context-persistence-jpa.xml",
-        "classpath*:META-INF/spring/context-persistence-jpa-hsql.xml",
+        "classpath*:META-INF/spring/context-persistence-jpa-h2.xml",
         "classpath*:META-INF/spring/context-transaction.xml"
 })
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
